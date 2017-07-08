e69209eff41f820b8381e1aa0112c5bff0a52d88
anytimesoon <damien@delartigue.co.uk>
2017-07-08 19:13:35 +0200

    users can register and log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	root 'pictures#index'
   devise_for :users
+  resource :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
