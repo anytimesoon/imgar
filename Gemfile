ee2c88b57c92cddd70e54cc4bf57b38475aef6ee
anytimesoon <damien@delartigue.co.uk>
2017-07-09 17:23:27 +0200

    first test passing. test db setup

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem 'selenium-webdriver'
   # gem 'rspec'
   gem 'rspec-rails'
+  gem 'pry'
 end
 
 group :development do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # before(:each) do
-  # 	@user = User.create()
-  # end
+  before(:each) do
+  	@user = User.first
+  end
 
   it "has many pictures" do
   	expect(@user.pictures.count).to eq(5)
