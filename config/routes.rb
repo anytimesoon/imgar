009bb0a1f3d70a92489548c7f6d2b388938bd02f
anytimesoon <damien@delartigue.co.uk>
2017-07-10 18:00:20 +0100

    users can sign up, log in, sign out and view profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	root 'pictures#index'
-  devise_for :users
-  resource :users, only: [:show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+ 	get 'users/:id' => 'users#show', as: :user
 end
