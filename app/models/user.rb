f33c0ab7af80effe62a5540bbc0cc96923dd0216
anytimesoon <damien@delartigue.co.uk>
2017-07-14 09:37:29 +0100

    users can log in with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,33 @@ class User < ApplicationRecord
   has_many :pictures
   has_many :comments
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates_format_of :name, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validate :validate_username
+
+	def validate_username
+	  if User.where(email: username).exists?
+	    errors.add(:name, :invalid)
+	  end
+	end
+
+  attr_accessor :login
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.name || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:name) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	root 'pictures#index'
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users#, :controllers => { registrations: 'registrations' }
 
  	get 'users/:id' => 'users#show', as: :user
 end
