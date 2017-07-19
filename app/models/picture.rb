f2dfe2f049db46734722310c91420721eea7fdc5
anytimesoon <damien@delartigue.co.uk>
2017-07-19 10:59:57 +0200

    paperclip gem installed

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,4 +5,7 @@ class Picture < ApplicationRecord
 	has_many :tags, through: :picture_tags
 
 	validates :path, presence: true
+
+	has_attached_file :path, styles: { medium: "500x500>", small: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :path, content_type: /\Aimage\/.*\z/
 end
