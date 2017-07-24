dde903a048cd61212f439fe90a31208c334265c5
anytimesoon <damien@delartigue.co.uk>
2017-07-24 22:19:35 +0100

    pictures views and controller ready to for uploads

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,31 @@
 class PicturesController < ApplicationController
 	def index
+		@pictures = Picture.all
+	end
+
+	def new
+		@picture = Picture.new
+	end
+
+	def create
+		@picture = Picture.new(picture_params)
+		@picture.user_id = current_user.id
+		
+		if @picture.save
+			redirect_to picture_path(@picture)
+		else
+			flash[:notice] = "Something went wrong, please try again"
+			redirect_to request.referer
+		end
+	end
+
+	def show
+		@picture = Picture.find(params[:id])
+	end
+
+	private
+
+	def picture_params
+		params.require(:picture).permit(:path)
 	end
 end
