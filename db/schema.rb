13721bc606de130aabb9231cd9d3230037fc0d2c
anytimesoon <damien@delartigue.co.uk>
2017-07-25 13:58:56 +0100

    add slug to tag migration

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170724164440) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
+    t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
