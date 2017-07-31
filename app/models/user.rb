734127ec0c4e24a20cd80f9b5f1515fe7bb7bf08
anytimesoon <damien@delartigue.co.uk>
2017-07-31 17:44:31 +0100

    Oauth for facebook working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :pictures
   has_many :comments
@@ -35,5 +36,13 @@ class User < ApplicationRecord
       where(conditions.to_hash).first
     end
   end
+
+  def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.name = auth.info.name 
+  end
+end
 end
 
