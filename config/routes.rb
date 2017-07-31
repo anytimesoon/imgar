517473d9875f49a2d5ee558f28a41c85976a35e7
anytimesoon <damien@delartigue.co.uk>
2017-07-31 11:08:53 +0100

    images now link to full size

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 	root 'pictures#index'
 
-  devise_for :users#, :controllers => { registrations: 'registrations' }
+  devise_for :users
 
  	get 'users/:id' => 'users#show', as: :user
 
  	resources :pictures, except: [:index] do
+ 		# get '/:title' => 'pictures#full', param: :title
  		post 'comments/create' => 'comments#create'
  	end
- 	
+
+
  	resources :tags, only: [:show], param: :slug
 
 end
