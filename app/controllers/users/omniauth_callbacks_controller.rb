b4e57a111e662ba6eb98671ddc77d73a4fece392
anytimesoon <damien@delartigue.co.uk>
2017-08-16 16:37:08 +0100

    pictures can only be sorted if there are pictures

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      binding.pry
       redirect_to new_user_registration_url
     end
   end
